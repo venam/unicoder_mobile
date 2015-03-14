b13064a0c92f1dd118d3646958c3a8c03a6ca4f3
venam <patrick@iotek.org>
2015-03-14 10:47:37 +0200

    unicoder mobile version

---
+++ README.md
@@ -0,0 +1,16 @@
+Unicoder Mobile Version
+-----------------------
+
+A Mobile version of [unicoder](https://github.com/venam/unicoder) also using Google material design.
+
+Some character are not supported by the default font.
+
+There are still things that needs to be fixed.
+
+
+Screenshots
+-----------
+
+![scrot1](https://raw.github.com/venam/unicoder_mobile/master/unicoder1.png)
+![scrot2](https://raw.github.com/venam/unicoder_mobile/master/unicoder2.png)
+
---
+++ app/src/main/java/net/nixers/venam/unicoder/MainActivity.java
@@ -0,0 +1,195 @@
+package net.nixers.venam.unicoder;
+
+import org.jtr.transliterate.*;
+
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.res.Configuration;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MainActivity extends ActionBarActivity {
+    private List<CharacterReplacer> allReplacers;
+    private ClipboardManager myClipboard;
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+
+    private int []allButtons = {
+            R.id.original,
+            R.id.one,
+            R.id.two,
+            R.id.three,
+            R.id.four,
+            R.id.five,
+            R.id.six,
+            R.id.seven,
+            R.id.eight,
+            R.id.nine,
+            R.id.ten,
+            R.id.eleven,
+            R.id.twelve,
+            R.id.thirteen,
+            R.id.fourteen,
+            R.id.fifteen
+    };
+
+    public MainActivity() {
+        String original = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~¡¢£¤¥¦§¨©ª«¬­®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþ";
+        this.allReplacers = new ArrayList<CharacterReplacer>();
+        try {
+            this.allReplacers.add(new CharacterReplacer(original,"!\"#$%&'()*+,-./0123456789:;<=>?@ДБҀↁЄFБНІЈЌLМИФРQЯЅГЦVЩЖЧZ[\\]^_`аъсↁэfБЂіјкlмиорqѓѕтцvшхЎz{|}~¡¢£¤¥¦§¨©ª«¬­®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþ"));
+            this.allReplacers.add(new CharacterReplacer(original,"!\"#$%&'()*+,-./0⑴⑵⑶⑷⑸⑹⑺⑻⑼:;<=>?@⒜⒝⒞⒟⒠⒡⒢⒣⒤⒥⒦⒧⒨⒩⒪⒫⒬⒭⒮⒯⒰⒱⒲⒳⒴⒵[\\]^_`⒜⒝⒞⒟⒠⒡⒢⒣⒤⒥⒦⒧⒨⒩⒪⒫⒬⒭⒮⒯⒰⒱⒲⒳⒴⒵{|}~¡¢£¤¥¦§¨©ª«¬­®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþ"));
+            this.allReplacers.add(new CharacterReplacer(original,"!\"#$%&'()*+,-./0߁23456789:⁏<=>⸮@AdↃbƎꟻGHIJK⅃MᴎOꟼpᴙꙄTUVWXYZ[\\]^_`AdↄbɘꟻgHijklmᴎoqpᴙꙅTUvwxYz{|}∽¡¢£¤¥¦§¨©ª«¬­®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþ"));
+            this.allReplacers.add(new CharacterReplacer(original,"!\"#$%&'()*+,-./0①②③④⑤⑥⑦⑧⑨:;<=>?@ⒶⒷⒸⒹⒺⒻⒼⒽⒾⒿⓀⓁⓂⓃⓄⓅⓆⓇⓈⓉⓊⓋⓌⓍⓎⓏ[\\]^_`ⓐⓑⓒⓓⓔⓕⓖⓗⓘⓙⓚⓛⓜⓝⓞⓟⓠⓡⓢⓣⓤⓥⓦⓧⓨⓩ{|}~¡¢£¤¥¦§¨©ª«¬­®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþ"));
+            this.allReplacers.add(new CharacterReplacer(original,"！\"＃＄％＆＇（）＊＋，－．／０１２３４５６７８９：；<＝>？＠ＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ[\\]^_`ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚ{|}~¡¢£¤¥¦§¨©ª«¬­®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþ"));
+            this.allReplacers.add(new CharacterReplacer(original,"!\"#$%&'()*+,-./𝟘𝟙𝟚𝟛𝟜𝟝𝟞𝟟𝟠𝟡:;<=>?@𝔸𝔹ℂ𝔻𝔼𝔽𝔾ℍ𝕀𝕁𝕂𝕃𝕄ℕ𝕆ℙℚℝ𝕊𝕋𝕌𝕍𝕎𝕏𝕐ℤ[\\]^_`𝕒𝕓𝕔𝕕𝕖𝕗𝕘𝕙𝕚𝕛𝕜𝕝𝕞𝕟𝕠𝕡𝕢𝕣𝕤𝕥𝕦𝕧𝕨𝕩𝕪𝕫{|}~¡¢£¤¥¦§¨©ª«¬­®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþ"));
+            this.allReplacers.add(new CharacterReplacer(original,"!\"#$%&'()*+,-./𝟎𝟏𝟐𝟑𝟒𝟓𝟔𝟕𝟖𝟗:;<=>?@𝐀𝐁𝐂𝐃𝐄𝐅𝐆𝐇𝐈𝐉𝐊𝐋𝐌𝐍𝐎𝐏𝐐𝐑𝐒𝐓𝐔𝐕𝐖𝐗𝐘𝐙[\\]^_`𝐚𝐛𝐜𝐝𝐞𝐟𝐠𝐡𝐢𝐣𝐤𝐥𝐦𝐧𝐨𝐩𝐪𝐫𝐬𝐭𝐮𝐯𝐰𝐱𝐲𝐳{|}~¡¢£¤¥¦§¨©ª«¬­®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþ"));
+            this.allReplacers.add(new CharacterReplacer(original,"!\"#$%&'()*+,-./0123456789:;<=>?@𝑨𝑩𝑪𝑫𝑬𝑭𝑮𝑯𝑰𝑱𝑲𝑳𝑴𝑵𝑶𝑷𝑸𝑹𝑺𝑻𝑼𝑽𝑾𝑿𝒀𝒁[\\]^_`𝒂𝒃𝒄𝒅𝒆𝒇𝒈𝒉𝒊𝒋𝒌𝒍𝒎𝒏𝒐𝒑𝒒𝒓𝒔𝒕𝒖𝒗𝒘𝒙𝒚𝒛{|}~¡¢£¤¥¦§¨©ª«¬­®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþ"));
+            this.allReplacers.add(new CharacterReplacer(original,"\"!\"#$%&'()*+,-܁/0123456789:;<=>?@ค๒ƈɗﻉिﻭɦٱﻝᛕɭ๓กѻρ۹ɼรՇપ۷ฝซץչ[\\]^_`ค๒ƈɗﻉिﻭɦٱﻝᛕɭ๓กѻρ۹ɼรՇપ۷ฝซץչ{|}~¡¢£¤¥¦§¨©ª«¬­®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþ"));
+            this.allReplacers.add(new CharacterReplacer(original,"!\"#$%&'()*+,-./0123456789:;<=>?@αв¢∂єƒﻭнιנкℓмησρ۹яѕтυνωχуչ[\\]^_`αв¢∂єƒﻭнιנкℓмησρ۹яѕтυνωχуչ{|}~¡¢£¤¥¦§¨©ª«¬­®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþ"));
+            this.allReplacers.add(new CharacterReplacer(original,"!\"#$%&'()*+,-./0123456789:;<=>?@ค๒ς๔єŦﻮђเןкɭ๓ภ๏קợгรՇยשฬאץչ[\\]^_`ค๒ς๔єŦﻮђเןкɭ๓ภ๏קợгรՇยשฬאץչ{|}~¡¢£¤¥¦§¨©ª«¬­®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþ"));
+            this.allReplacers.add(new CharacterReplacer(original,"!\"#$%&'()*+,⸚./012ӟ456789:;<=>?@ÄḄĊḊЁḞĠḦЇJḲḶṀṄÖṖQṚṠṪÜṾẄẌŸŻ[\\]^_`äḅċḋëḟġḧïjḳḷṁṅöṗqṛṡẗüṿẅẍÿż{|}~¡¢£¤¥¦§¨©ª«¬­®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþ"));
+            this.allReplacers.add(new CharacterReplacer(original,"!\"#$%&'()*+,-./0123456789:;<=>?@ÁBĆDÉFǴHíJḰĹḾŃŐṔQŔśTŰVẂXӲŹ[\\]^_`ábćdéfǵhíjḱĺḿńőṕqŕśtúvẃxӳź{|}~¡¢£¤¥¦§¨©ª«¬­®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþ"));
+            this.allReplacers.add(new CharacterReplacer(original,"!\"#$%&'()*+,-./0123456789:;<=>?@ᴀʙᴄᴅᴇꜰɢʜɪᴊᴋʟᴍɴᴏᴩQʀꜱᴛᴜᴠᴡxYᴢ[\\]^_`ᴀʙᴄᴅᴇꜰɢʜɪᴊᴋʟᴍɴᴏᴩqʀꜱᴛᴜᴠᴡxyᴢ{|}~¡¢£¤¥¦§¨©ª«¬­®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþ"));
+            this.allReplacers.add(new CharacterReplacer(original,"!\"#$%&'()*+,-./0123456789:;<=>?@ﾑ乃cd乇ｷgんﾉﾌズﾚﾶ刀oｱq尺丂ｲu√wﾒﾘ乙[\\]^_`ﾑ乃cd乇ｷgんﾉﾌズﾚﾶ刀oｱq尺丂ｲu√wﾒﾘ乙{|}~¡¢£¤¥¦§¨©ª«¬­®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþ"));
+
+
+
+        } catch (CharacterParseException c) {
+
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myClipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+
+
+        Toolbar t = new Toolbar(this);
+        t.setNavigationIcon(R.drawable.ic_launcher);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,                  /* host Activity */
+                mDrawerLayout,         /* DrawerLayout object */
+                t,  /* nav drawer icon to replace 'Up' caret */
+                R.string.drawer_open,  /* "open drawer" description */
+                R.string.drawer_close  /* "close drawer" description */
+        ) {
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                //getSupportActionBar().show();
+                getSupportActionBar().setTitle("Unicoder");
+            }
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                //getSupportActionBar().hide();
+                getSupportActionBar().setTitle("Unicoder Menu");
+            }
+        };
+        mDrawerToggle.setHomeAsUpIndicator(R.drawable.ic_launcher);
+        // Set the drawer toggle as the DrawerListener
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onSubmit(View view) {
+        //hide the soft keyboard from the screen
+        InputMethodManager mgr = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        mgr.hideSoftInputFromWindow(view.getWindowToken(), 0);
+
+        //get the text from the EditText box
+        EditText t = (EditText) findViewById(R.id.textInput);
+        String aInput = t.getText().toString();
+
+        //make sure we don't have an empty string
+        if (aInput.equalsIgnoreCase("") || aInput.equalsIgnoreCase(" ")) {
+            return;
+        }
+        if (allReplacers.size()+1 != allButtons.length) {
+            Toast.makeText(this, "Not same length: "+allReplacers.size(), Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "allButtons: "+allButtons.length, Toast.LENGTH_SHORT).show();
+        }
+
+        Button b0 = (Button) findViewById(allButtons[0]);
+        b0.setText(aInput);
+        for(int i=0; i<allReplacers.size(); i++){
+            Button b = (Button) findViewById(allButtons[i+1]);
+            String aResult = allReplacers.get(i).doReplacement( aInput );
+            b.setText(aResult);
+        }
+    }
+
+
+    public void onClear(View view) {
+        EditText t = (EditText) findViewById(R.id.textInput);
+        t.setText("");
+    }
+
+    public void onCopy(View view) {
+        Button b = (Button) view;
+        ClipData myClip = ClipData.newPlainText("unicoder", b.getText().toString());
+        myClipboard.setPrimaryClip(myClip);
+        Toast.makeText(this, b.getText().toString(), Toast.LENGTH_SHORT).show();
+
+    }
+}
